9e8c440b45fc7be9ad137ff150ecd8dbfab367ba
jekrb <jake@burden.blog>
2019-06-17 19:18:47 -0400

    use -i or --ignore for markup, scripts, or styles

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ fn main(args: cli::Args) -> Result<(), std::io::Error> {
     for component in args.component.iter() {
         templates::write_all(&component, &args)?;
     }
+
     Ok(())
 }
--- src/templates.rs
+++ src/templates.rs
@@ -7,35 +7,41 @@ static STYLES_FILE: &str = include_str!("../templates/styles.txt");
 static EMOTION_FILE: &str = include_str!("../templates/emotion.txt");
 
 pub fn write_all(component: &str, args: &super::cli::Args) -> Result<(), std::io::Error> {
-  fs::create_dir(&component)?;
+    fs::create_dir(&component)?;
 
-  if args.react {
-    let index_path = format!("./{}/index.js", &component);
-    fs::File::create(index_path)?;
+    if args.react {
+        let index_path = format!("./{}/index.js", &component);
+        fs::File::create(index_path)?;
 
-    let emotion_path = format!("./{}/styles.js", &component);
-    let path = Path::new(&emotion_path);
-    fs::write(path, EMOTION_FILE)?;
-  } else {
-    let markup_file = generate(MARKUP_FILE, &component);
-    let markup_path = format!("./{}/index{}", &component, &args.markup_extension);
-    let path = Path::new(&markup_path);
-    fs::write(path, markup_file)?;
+        let emotion_path = format!("./{}/styles.js", &component);
+        let path = Path::new(&emotion_path);
+        fs::write(path, EMOTION_FILE)?;
+    } else {
+        if !args.ignore.contains(&"markup".to_string()) {
+            let markup_file = generate(MARKUP_FILE, &component);
+            let markup_path = format!("./{}/index{}", &component, &args.markup_extension);
+            let path = Path::new(&markup_path);
+            fs::write(path, markup_file)?;
+        }
 
-    let styles_file = generate(STYLES_FILE, &component);
-    let styles_path = format!("./{}/styles{}", &component, &args.styles_extension);
-    let path = Path::new(&styles_path);
-    fs::write(path, styles_file)?;
+        if !args.ignore.contains(&"styles".to_string()) {
+            let styles_file = generate(STYLES_FILE, &component);
+            let styles_path = format!("./{}/styles{}", &component, &args.styles_extension);
+            let path = Path::new(&styles_path);
+            fs::write(path, styles_file)?;
+        }
 
-    let scripts_file = generate(SCRIPTS_FILE, &component);
-    let scripts_path = format!("./{}/index{}", &component, &args.scripts_extension);
-    let path = Path::new(&scripts_path);
-    fs::write(path, scripts_file)?;
-  }
+        if !args.ignore.contains(&"scripts".to_string()) {
+            let scripts_file = generate(SCRIPTS_FILE, &component);
+            let scripts_path = format!("./{}/index{}", &component, &args.scripts_extension);
+            let path = Path::new(&scripts_path);
+            fs::write(path, scripts_file)?;
+        }
+    }
 
-  Ok(())
+    Ok(())
 }
 
 fn generate(template: &str, component: &str) -> String {
-  str::replace(template, "{{Component}}", &component)
+    str::replace(template, "{{Component}}", &component)
 }
