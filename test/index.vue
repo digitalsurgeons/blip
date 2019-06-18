feee46c521847a11fc424353de3f0e2ec7f90f32
Cory ZIbell <cory.zibell@digitalsurgeons.com>
2019-06-18 13:20:27 -0400

    Add VueJS Support. Closes 8.

---
+++ test/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="test">
+
+  </section>
+</template>
+
+<script>
+module.exports = {
+  name: "test",
+  props: {
+
+  },
+  data: () => {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  watch: {
+
+  }
+}
+</script>
+
+<style scoped>
+.test {
+  
+}
+</style>
